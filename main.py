f16313f28429f3bbebf05e0483d0b28173ee07a0
ngo500 <72844093+ngo500@users.noreply.github.com>
2021-11-14 10:39:04 -0800

    Update main.py
    updated main to call functions properly- uses api for the current price, and uses ai api for historical low price

--- main.py
+++ main.py
@@ -2,19 +2,42 @@ from config import config
 from userinput import userinput
 from airplane import airplane
 from amadeus import Client, ResponseError
+import json
 
-start = config()
-baseURL = start.getBaseURL()
-amadeus = start.getClient()
-input = userinput()
-params = input.getParam()
-parseAir = airplane()
+class main:
+    def __init__(self):
+        main = ""
 
-try:
-    response = amadeus.shopping.flight_offers_search.get(**params)
-    #print(response.data)
-    parseAir.parse(response.data)
+    def runMain(self, event):
+        start = config()
+        baseURL = start.getBaseURL()
+        amadeus = start.getClient()
+        input = userinput()
+        parseAir = airplane()
 
-except ResponseError as error:
-    print("error! ")
-    print (error)
+        inputInfo = []
+        for (x, y) in event.items():
+            inputInfo.append(str(y))
+
+        input.setOrigin(inputInfo[0])
+        input.setDestination(inputInfo[1])
+        input.setArrivalDate(inputInfo[2])
+
+        try:
+            params = input.getOldParam()
+            response = amadeus.analytics.itinerary_price_metrics.get(**params)
+            olddata = parseAir.parseOld(response.data)
+
+            params = input.getCurParam()
+            response = amadeus.shopping.flight_offers_search.get(**params)
+            newdata = parseAir.parseCur(response.data)
+            
+            data = {
+                "oldValue": olddata,
+                "newValue": newdata
+            }
+            return data
+
+        except ResponseError as error:
+            print("error! ")
+            print (error)
